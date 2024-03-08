573619ad2cae1c4c3670013cc42d4d2b7baadc92
Sandra Taskovic <staskovi@ualberta.ca>
2024-03-08 12:10:16 -0700

    sandra fixes failed addCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -55,10 +55,10 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void addCity(City city){
-        /*if (cities.contains(city)) {
+        if (cities.contains(city)) {
             throw new IllegalArgumentException();
         }
-        cities.add(city);*/
+        cities.add(city);
     }
 
 }
